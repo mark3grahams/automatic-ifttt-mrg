ba1e66eba515a67a7cf622b298f00964b30ba447
mark3grahams <mark@3grahams.com>
2016-08-18 13:29:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -38,6 +38,8 @@ app.get('/', function(req, res) {
             '<div class=\'inner\'>' +
             '<h1>' + lastFuelReading + '%</h1>' +
             '<p>fuel remaining</p>' +
+            '<h1>' + lastFuelReading + '%</h1>' +
+            '<p>fuel remaining plus 10</p>' +
             '</div>' +
             '</div>' +
             '</div>' +
